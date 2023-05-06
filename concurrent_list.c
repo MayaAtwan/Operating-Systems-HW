35a6d55653bb739844d2036a3e288e8f9bfe7755
Maya Atwan <maya.atwan.2000@gmail.com>
2023-05-06 10:14:31 +0300

    Update concurrent_list.c

--- concurrent_list.c
+++ concurrent_list.c
@@ -4,7 +4,6 @@
 #include <limits.h>
 #include "concurrent_list.h"
 
-// it's mahmoud from GitHub
 struct node {
     int value;
     node* next;
@@ -13,6 +12,7 @@ struct node {
 
 struct list {
     node* head;
+    pthread_mutex_t locked;
 };
 
 void print_node(node* node)
@@ -24,8 +24,7 @@ void print_node(node* node)
     }
 }
 
-
-// this function creates a new embty linked list, the return value:pointer to  new linked list.
+// this function creates a new embty linked list, the return value:pointer to new linked list.
 list* create_list()
 {
     //allocate memory
@@ -35,46 +34,47 @@ list* create_list()
         exit(1);
     // if we are here then malloc succeded, so initializes the head to Null
     List->head = NULL;
+    // initilize the lock and check it
+    if (pthread_mutex_init(&List->locked, NULL) != 0)
+    {
+        free(List);
+        exit(1);
+    }
     //returns a pointer to the new List struct
-
     return List;
 }
 
+//Deletes a linked list and frees all of its associated memory.
 void delete_list(list* list)
 {
-    //if the list in null
+    //if the list in null,there is nothing to delete
     if (list == NULL)
         return;
     node* node_next;
     node* curr = list->head;
-    //if curr was removed, so check that there is element in the curr(if the list is empty)
+    // If the list is empty, free the memory associated with the list itself
     if (curr == NULL)
     {
+        // Destroy the mutex associated with the list and free the memory
+        pthread_mutex_destroy(&list->locked);
         free(list);
         return;
     }
-    // if we are here then the node must to be deleted so
-    //locked it
-    pthread_mutex_lock(&curr->locked);
     while (curr)
     {
+        // Keep track of the next node before freeing the current one
         node_next = curr->next;
-        if (node_next)
-        {
-            pthread_mutex_lock(&node_next->locked);
-        }
-        pthread_mutex_unlock(&curr->locked);
+        // Destroy the mutex associated with the current node and free the memory
         pthread_mutex_destroy(&curr->locked);
         free(curr);
+        // Update the curr
         curr = node_next;
     }
-    list->head = NULL;
+    pthread_mutex_destroy(&list->locked);
     free(list);
-
 }
 
 
-//the new node is not yet part of the list and there is no contention for locks.
 // in this function we allocating memory for the node, checking if malloc failed,if not
 //initialize the fields of the node.
 node* create_node(int value)
@@ -95,40 +95,48 @@ node* create_node(int value)
     return new_node;
 }
 
+
+//Inserts a new node into the linked list in sorted order, with the given value.
 void insert_value(list* list, int value)
 {
     if (list) {/*make sure they did not pass a NULL as list*/
+        // create new node through the function create_node
         node* new_node = create_node(value);
         node* prev = NULL;
         node* curr = NULL;
 
-        pthread_mutex_lock(&new_node->locked);
-        //if the list is empty
+        // locked the list to check the head, we locked the list in 2 cases if we must to change the head
+        // or if the list is empty
+        pthread_mutex_lock(&list->locked);
+
+        //if the list is empty so the insert node become the head of the list.
         if (list->head == NULL)
         {
-            /*pthread_mutex_lock((&list->head->locked)); (list->head is NULL)!! we get into this block only if  list->head == NULL so list->head->locked won't work */
             new_node->next = NULL;
             list->head = new_node;
-            /*pthread_mutex_unlock((&list->head->locked));*/
-            pthread_mutex_unlock((&new_node->locked));
+            pthread_mutex_unlock((&list->locked));
+
         }
 
         //if the new node is the new head(minimal value)
-        else if (value < list->head->value)
+        // we locked the head and the list to change the head to the new_node
+        else if (value <= list->head->value)
         {
             pthread_mutex_lock((&(list->head)->locked));
+
             new_node->next = list->head;
             list->head = new_node;
-            pthread_mutex_unlock((&(list->head)->locked));
-            pthread_mutex_unlock((&new_node->next->locked));/*was before correction : pthread_mutex_unlock((&new_node->locked)); you unlocked list->head twice  */
+            pthread_mutex_unlock((&new_node->next->locked));
+            pthread_mutex_unlock(&list->locked);
         }
         else
-        {
-            //find where to insert the new node.
+        {   // if we are here then we must to insert the new node between 2 nodes or at the end
+            //find where to insert the new node, we used hand over hand locking method.
             prev = list->head;
             curr = prev->next;
             pthread_mutex_lock(&(prev->locked));
-            while (curr != NULL && curr->value < value)
+            pthread_mutex_unlock(&list->locked);
+            while (curr != NULL && curr->value <= value)
             {
                 pthread_mutex_lock(&(curr->locked));
                 pthread_mutex_unlock(&(prev->locked));
@@ -139,7 +147,7 @@ void insert_value(list* list, int value)
             new_node->next = curr;
             prev->next = new_node;
             pthread_mutex_unlock(&(prev->locked));
-            pthread_mutex_unlock(&(new_node->locked));
+            //If curr is not NULL, it was locked in the while loop and should be unlocked now
             if (curr != NULL) {
                 pthread_mutex_unlock(&(curr->locked));
             }
@@ -151,10 +159,23 @@ void remove_value(list* list, int value)
 {
     node* prev = NULL;
     node* curr = NULL;
-    if (list && list->head) {/*make sure they did not pass a NULL or empty list as a variable*/
+    //if the list is NULL
+    if (!list)
+    {
+        return;
+    }
+    pthread_mutex_lock(&(list->locked));
+    //if the list is empty
+    if (!(list->head))
+    {
+        pthread_mutex_unlock(&list->locked);
+        return;
+    }
+    else{
         prev = list->head;
         curr = prev->next;
         pthread_mutex_lock(&(list->head)->locked);
+        pthread_mutex_unlock(&list->locked);
         //if the wanted node is the head(minimal value)
         if (value == prev->value)
         {
@@ -189,26 +210,31 @@ void remove_value(list* list, int value)
 // print function ~ print the list
 void print_list(list* list)
 {
-    //if there is no list
+    //if the list is NULL
     if (list == NULL)
     {
         printf("\n");
         return;
     }
+    pthread_mutex_lock(&list->locked);
     //if the list is empty
     if (list->head == NULL)
     {
+        pthread_mutex_unlock(&list->locked);
         printf("\n");
         return;
     }
-    // each time we lock one node print it then lock the next after that unlock the node
+    // using hand over hand locking
+    // each time we lock one node print it then lock the next after that,unlock the node
     //1)lock curr node.
     //2)print curr node.
-    //3)lock node->next
+    //3)lock curr->next
     //4)unlock curr node
+    //
     node* curr = list->head;
     node* next;
     pthread_mutex_lock(&(curr->locked));
+    pthread_mutex_unlock(&list->locked);
     while (curr != NULL) {
         print_node(curr);
         next = curr->next;
@@ -224,14 +250,28 @@ void print_list(list* list)
     return;
 }
 
-
 void count_list(list* list, int (*predicate)(int))
 {
     int count = 0; // DO NOT DELETE
-    if (list && list->head) {
+    //if the list is NULL
+    if (!list) {
+        printf("%d items were counted\n", count);
+        return;
+    }
+    pthread_mutex_lock(&list->locked);
+    //if the list is empty
+    if (!list->head) {
+        printf("%d items were counted\n", count);
+        pthread_mutex_unlock(&list->locked);
+        return;
+
+    }
+    else {
         node* curr = list->head;
         node* next;
+        //// using hand over hand locking
         pthread_mutex_lock(&(curr->locked));
+        pthread_mutex_unlock(&list->locked);
         while (curr != NULL) {
             if (predicate(curr->value)) {
                 count++;
@@ -242,7 +282,7 @@ void count_list(list* list, int (*predicate)(int))
                 pthread_mutex_lock(&(next->locked));
             }
             pthread_mutex_unlock(&(curr->locked));
-            curr = next;// this line was missing in your code
+            curr = next;
         }
     }
 
